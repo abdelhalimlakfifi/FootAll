bc083f1fdf1e452aac5794ace6b1350dae233287
Se0A0eM <76744964+Se0A0eM@users.noreply.github.com>
2023-09-04 09:15:45 +0100

    adding blog page

---
+++ dist/OurBlogs/assets/js/blogs.js
@@ -0,0 +1,72 @@
+export default [
+    {
+        date: 'Sep 02, 2023',
+        tag: 'La Liga',
+        title: 'New Golden Boy?',
+        description: `On the opener of Santiago Bernabio stadium, Los Blancos faced Getafe on the 4th round of LaLiga, Getafe and against all expectition take the lead thanks to Borja Mayoral in the 11th minute, after the break Real Madrid showed up their true colors with an equalizir by Joselu in the 47th minute, on the additional time the player of the month in LaLiga Bellingham scores the winning goal on the 95th minute.\nJude became the second player to score in each of Real Madrid first four LaLiga matches after Cristiano Ronaldo.\nWhile Getafe battled valiantly, it was the precocious talent of Bellingham that sparked Madrid's comeback and ensured the Royal House's new stadium got the perfect opening result. His recent displays have surely cemented his status as England's new 'Golden Boy'.`,
+        img: 'Bellingham.jpg'
+    },
+    {
+        date: 'Sep 02, 2023',
+        tag: 'Premier League',
+        title: 'The Terminator Earling Haaland',
+        description: `Man City's relentless scoring machine Erling Haaland continued his devastating form against Fulham, registering a sensational hat-trick in City's 5-1 win.\nHaaland set up Julian Alvarez to open the scoring in the 31st minute, teeing up his teammate with a precise pass. Fulham hit back immediately through Tim Ream to make it 1-1 heading into half-time. City regained the lead just before the break thanks to Nathan Ake.\nThe visitors had no answer for Haaland after the restart. In the 59th minute, The Norwegian hitman drilled a powerful strike into the bottom corner. 12 minutes later, Haaland grabbed his second, finishing off a clinical team move started and finished by him and Alvarez.\nWith the game already well in City's control, Haaland completed his hat-trick in stoppage time.\nHaaland's incredible form continues and City takes again thier place at the top of the Premier League table.`,
+        img: 'HaalandTerminator.jpg'
+    },
+    {
+        date: 'Sep 02, 2023',
+        tag: 'Premier League',
+        title: 'Son Is Back?',
+        description: `After speculation around his place in Spurs starting XI, Heung-Min Son silenced his critics with a stellar hat-trick performance to lead Tottenham to a 5-2 victory over Burnley.\nIt was Burnley who took a shocking early lead through Foster in the 4th minute. Son quickly brought Spurs level with a well-taken finish in the 16th.\nRomero then put the spurs in front just before half-time. Son's teammates started the second half strong as it took Maddison only 9 minutes make it 3-1.\nSon completed his brace in the 63th minute. The South Korean was unplayable and grabbed his hat-trick just minutes later to seal the victory.\nWhile Burnley grabbed a late consolation, this was all about Son, who answered his doubters in emphatic fashion with an eye-catching display full of skill, hard work and finishing prowess. After some speculation around his place in the side, this man of the match performance proved Son remains critical to Spurs' ambitions.`,
+        img: 'HMS.jpg'
+    },
+    {
+        date: 'Sep 02, 2023',
+        tag: 'Premier League',
+        title: 'Brighton Symphony',
+        description: `Roberto De Zerbi's exciting Brighton side produced another sublime performance to defeat Newcastle 3-1.\nDe Zerbi's team took control early on through Ferguson's well-taken opener on 27 minutes, exquisitely set up by Gilmour.\nBrighton continued to dominate possession and showcase their fluid attacking style, and were rewarded further in the second half. Ferguson notched his brace in the 65th minute courtesy of another Gilmour assist.\nThe winger completed his hat-trick just 5 minutes later, with Mitoma assist, to send the Brighton fans into dreamland.\nNewcastle grabbed a late consolation through Wilson but it was too little, too late for Eddie Howe's men.\nThis was truly the Brighton symphony at its best - a fluid, attacking masterclass led by man of the match Ferguson whose clinical finishing epitomised De Zerbi's team's eye-catching style. The Seagulls continue to astound with their possession-based gameplay, and cemented their place as one of the league's most exciting teams to watch.`,
+        img: 'Brighton.jpg'
+    },
+    {
+        date: 'Sep 02, 2023',
+        tag: 'Saudi Pro League',
+        title: 'CR7 Still Reigns Supreme',
+        description: `Even at the age of 37, Cristiano Ronaldo continues to defy father time with his unquenchable goalscoring thirst. After 5 rounds of the Saudi Pro League Roshn, the prolific Portuguese leads the scoring charts with 6 goals.\nRonaldo entered Round 5 still out in front, but with Hamadallah taking over him after scoring in the classico clash with Al-Hilal. However, CR7 had no intentions of relinquishing his golden boot as he struck in the 68th minute of Al-Nassr's commanding 5-1 victory over Al Hazm to retain his place at the summit.\nDespite the challenge of Hamadallah, Ronaldo has remained the most clinical operator in front of goal so far.\nAfter 5 matches, the five-time Ballon d'Or winner's supremacy remains unchecked as he carries his legendary goalscoring prowess into a new league and chapter of his illustrious playing days.`,
+        img: 'CR7.jpg'
+    },
+    {
+        date: 'Sep 02, 2023',
+        tag: 'Bundesliga',
+        title: 'Bayern Win As Usual!',
+        description: `FC Bayern München visited Borussia Mönchengladbach on the 3rd round of Bundesliga.\nthe match started with quick attacks from BMG players, and they transleted their attacks into the 1st goal in the 30th minute by Itakura. The champions waited until the 58th minute to scores the equalizer thanks to Sane. Just when it seemed the match would end in a draw, Tel came off the bench to have an instant impact.\n In the 87th minute on the dot, Bayern scored the late winning goal to maintain their unbeaten start and lift themselves back into second place behind Bayern Leverkusen on goals scored.`,
+        img: 'BayernMunchen.jpg'
+    },
+    {
+        date: 'Sep 02, 2023',
+        tag: 'Serie A',
+        title: 'Champions chock',
+        description: `The Serie A champions Napoli hosted Lazio in a highly competitive fixture as Maurizio Sarri's team had been in strong form of late.\nThe intense match started with both sides trading attacks until the 30th minute, when Alberto gave Lazio the lead. However, Napoli replied swiftly and drew level within two minutes through Zielinski's equaliser. The second half remained tightly contested until the 52nd minute, when Kamada restored Lazio's advantage.\nWhile there were no further goals, it was a compelling contest between two in-form outfits that ended with the points for Lazio.`,
+        img: 'Lazio.jpg'
+    },
+    {
+        date: 'Sep 03, 2023',
+        tag: 'Premier League',
+        title: 'Classico Comeback',
+        description: `Emirates Stadium hosted a classic clash of Premier League between Arsenal and Manchester United, The match started with quick attacks from both sides.\nThe visitors struck first through Marcus Rashford in the 27th minute, finishing coolly after a slick team move. However, their advantage was short-lived as just moments later Martin Ødegaard popped up unmarked to level with an instinctive finish.\nThe contest remained in the balance throughout the second period as chances came and went for either team. As the full-time whistle loomed, Mikel Arteta's men launched a dramatic late assault.\nIn the  97th minute, summer signing Declan Rice scores his first goal on arsenal shirt and given his team the lead. 3minutes later Gabriel Jesus add a 3rd goal.\nThe Red Devils back to their home with the 2nd defeat of the new season and locating at the middle of the table, while The Gunners are now on the 5th place.`,
+        img: 'Arsenal.jpg'
+    },
+    {
+        date: 'Sep 03, 2023',
+        tag: 'Serie A',
+        title: 'Inter Steal Top Spot From ACM',
+        description: `Directly before their clash in the next round, Inter Milan stole the 1st spot in the Serie A table from rivals AC Milan. Thanks to their emphatic 4-0 win over Fiorentina.\nThuram opened the match with an early goal at the 24th minute. After the break, Lautaro Martinez doubled Inter's lead with a cool finish in the 53rd minute. Hakan Calhanoglu increased their advantage further just 5 minutes later from the penalty spot. Lautaro wasn't done yet, completing his brace in the second half to make it 4-0.\nThe commanding victory sees Inter seize top spot ahead of their upcoming Derby with Milan.`,
+        img: 'Inter.jpg'
+    },
+    {
+        date: 'Sep 03, 2023',
+        tag: 'Ligue 1',
+        title: 'PSG wake up',
+        description: `After dropping some points in their first league rounds, PSG woke up with a smashing win in the Classico over Lyon.\n The match changed early with Mbappe scoring in the 4th minute, Hakimi added the second goal in the 20th minute. Then Asensio showed up by scoring the 3rd goal for Enerique's team. On the additional time of the first half Mbappe completed his brace and made it 4-0 for his team. After the break, Tolisso scored a consolation goal for Lyon that did nothing to change the outcome.\nPSG won the Classico convincingly and announced their title intentions.`,
+        img: 'PSG.jpg'
+    }
+]
---
+++ dist/OurBlogs/assets/js/script.js
@@ -0,0 +1,32 @@
+import blogs from './blogs.js';
+document.addEventListener('DOMContentLoaded', () => {
+    const blogsContainer = document.querySelector('#blogsContainer');
+    for (let i = blogs.length - 1; i >= 0; i--) {
+        const article = document.createElement('article');
+        article.classList.add('flex');
+        article.classList.add('justify-between');
+        article.classList.add('items-start');
+        article.classList.add('max-w-xl');
+        article.innerHTML = `<div
+        class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+        <img class="rounded-t-lg h-96 w-full object-cover" src="./assets/img/${blogs[i].img}" alt="" />
+    </a>
+    <div class="p-5">
+        <div class="flex items-center justify-between gap-x-4 text-xs">
+            <time datetime="2020-03-16" class="text-gray-500">${blogs[i].date}</time>
+            <a href="#"
+                class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">${blogs[i].tag}</a>
+        </div>
+        <div class="group relative">
+            <h3
+                class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600 text-center uppercase">
+                <a href="#">
+                    ${blogs[i].title}
+                </a>
+            </h3>
+            <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">${blogs[i].description}</p>
+            </div>`;
+        blogsContainer.appendChild(article);
+    };
+});
